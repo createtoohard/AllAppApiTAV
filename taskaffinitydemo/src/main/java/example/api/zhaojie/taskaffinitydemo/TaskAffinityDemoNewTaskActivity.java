86ba0bff231ea7c87e54a510d3fe6dc65b92175c
zhaojie <zhaojie4_4@163.com>
2018-06-21 13:57:11 +0800

    continue taskAffinity attru test add FLAG_ACTIVITY_NEW_TASK case
    Signed-off-by: zhaojie <zhaojie4_4@163.com>

---
+++ taskaffinitydemo/src/main/java/example/api/zhaojie/taskaffinitydemo/TaskAffinityDemoNewTaskActivity.java
@@ -0,0 +1,17 @@
+package example.api.zhaojie.taskaffinitydemo;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+
+public class TaskAffinityDemoNewTaskActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.taskaffinitynewtask);
+    }
+}
